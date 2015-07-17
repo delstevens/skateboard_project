e325374ef291956727d3dd34062a73e49376df1d
IonaCS <ionakat@gmail.com>
2015-07-17 15:04:56 +1200

    clean up view file

--- view.rb
+++ view.rb
@@ -1,24 +1,6 @@
-# class View
-
-#   def initialize
-
-#   end
-
-#   def render_skateboard(item)
-#     p item
-#     this_array = ["N", "P", "X"]
-#     return this_array[rand(3)]
-#   end
-# end
-
-
 class View
 
-  def initialize
-
-  end
-
-  def render_skateboard skateboard
+  def render_skateboard(skateboard)
     deck = render_deck(skateboard.length.to_i)
     wheels = render_wheels(skateboard.length.to_i)
     render(skateboard.name, skateboard.length.to_i, skateboard.cost, deck, wheels)
@@ -26,8 +8,7 @@ class View
   end
 
   def render_deck(length)
-    string ="▀▀▀▀▀" + ("█"*(length/1.2)) + "▀▀▀▀▀"
-    return string
+    return "▀▀▀▀▀" + ("█"*(length/1.2)) + "▀▀▀▀▀"
   end
 
 
@@ -35,44 +16,24 @@ class View
     return "  (_) " + (" "*length) + " (_)  "
   end
 
-
-
-# -══τ═══════════τ═══-
-#     (O)            (O)
-
-
-
-#  ▀▀▀▀▀████████████████████████████████████████▀▀▀▀▀▀            █ ▄ ▀ ▐  ▌ -
-#    (_)                                                     (_)
-
-
-#  ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-#   ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
-
   def render name, length, cost, deck, wheels
-
     puts "************************************************"
-    puts "*       Model: #{name}                         "
-    puts "*       Length: #{length} inches                      "
-    puts "*       Cost: #{cost}                          "
-    puts "*                                              "
-    puts "*                                              "
-    puts "*                                              "
-    puts "*                                              "
-    puts "*                                              "
-    puts "*                                              "
+    puts "       Model: #{name}                         "
+    puts "       Length: #{length} inches                      "
+    puts "       Cost: #{cost}                          "
+    puts "                                              "
+    puts "                                              "
+    puts "                                              "
+    puts "                                              "
+    puts "                                              "
+    puts "                                              "
     puts deck
     puts wheels
-    puts "*                                              "
-    puts "*                                              "
-    puts "*                                              "
-    puts "*                                              "
+    puts "                                              "
+    puts "                                              "
+    puts "                                              "
+    puts "                                              "
     puts "************************************************"
     puts "    [P]:previous    [N]:next      [X]:exit      "
-
   end
-
-
-
 end
-
