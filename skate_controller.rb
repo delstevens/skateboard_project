5be646e6e043e09959d16922ef4da933d3853847
Apprentice <apprentice@StMac0.local>
2015-07-17 12:19:14 +1200

    controller working with IO and search function

---
+++ skate_controller.rb
@@ -0,0 +1,23 @@
+require './quiver.rb'
+require './view.rb'
+
+
+class Controller
+
+
+  attr_reader :quiver
+
+  def initialize
+    @quiver = Quiver.new("board_data")
+    @board_view = View.new
+  end
+
+end
+
+board_database = Controller.new
+
+p board_database.quiver.skateboards_array
+p board_database.quiver.search_by_price(200)
+
+
+
---
+++ view.rb
@@ -0,0 +1,7 @@
+class View
+
+  def initialize
+
+  end
+
+end
